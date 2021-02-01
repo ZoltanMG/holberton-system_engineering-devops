16cd3f46c367d27fc2a9dcd487315540d7d398a6
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-02-01 22:10:20 +0000

    [update]: valide name of file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,18 +20,12 @@ if __name__ == "__main__":
     url_todo = '{}todos?userId={}'.format(url, id)
     todo = requests.get(url_todo)
     todo = todo.json()
-    all_tasks = len(todo)
 
-    url_completed = '{}todos?userId={}&completed=true'.format(url, id)
-    all_completed = requests.get(url_completed)
-    all_completed = all_completed.json()
-    completed_tasks = len(all_completed)
-
-    name_file = '{}.cvs'.format(id)
-    with open(name_file, 'w', encoding='utf-8') as csv_file:
+    name_file = '{}.csv'.format(id)
+    with open(name_file, 'w') as csv_file:
         csv_writer = csv.writer(csv_file)
         for do in todo:
-            csv_writer.writerow(["{}".format(id),
-                                "{}".format(name_user),
-                                "{}".format(do.get('completed')),
-                                 "{}".format(do.get('title'))])
+            csv_writer.writerow([str(id),
+                                name_user,
+                                do.get('completed'),
+                                do.get('title')])
