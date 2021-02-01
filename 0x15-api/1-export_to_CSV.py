901f3bce0ec193d7d9ae11ab9003fb3589d80575
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-02-01 21:49:45 +0000

    [add]: Exportar a CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+use REST API {JSON} Placeholder
+Gather data from an API.
+"""
+
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = requests.get('{}users/{}'.format(url, id))
+    user = user.json()
+    name_user = user.get('name')
+
+    url_todo = '{}todos?userId={}'.format(url, id)
+    todo = requests.get(url_todo)
+    todo = todo.json()
+    all_tasks = len(todo)
+
+    url_completed = '{}todos?userId={}&completed=true'.format(url, id)
+    all_completed = requests.get(url_completed)
+    all_completed = all_completed.json()
+    completed_tasks = len(all_completed)
+
+    name_file = '{}.cvs'.format(id)
+    with open(name_file, 'w', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for do in todo:
+            csv_writer.writerow([id, name_user,
+                                do.get('completed'),
+                                do.get('title')])
