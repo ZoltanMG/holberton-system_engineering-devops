ec46ae3ed8b4a7927729c377a6c9cdd87192d378
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-02-01 20:39:45 +0000

    [add]: 0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+use REST API {JSON} Placeholder
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user = requests.get('{}users/{}'.format(url, id))
+    user = user.json()
+    name_user = user.get('name')
+
+    url_todo = '{}todos?userId={}'.format(url, id)
+    todo = requests.get(url_todo)
+    todo = todo.json()
+    all_tasks = len(todo)
+
+    url_completed = '{}todos?userId={}&completed=true'.format(url, id)
+    all_completed = requests.get(url_completed)
+    all_completed = all_completed.json()
+    completed_tasks = len(all_completed)
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name_user,
+        completed_tasks,
+        all_tasks))
+    for do in all_completed:
+        print("\t {}".format(do.get('title')))
